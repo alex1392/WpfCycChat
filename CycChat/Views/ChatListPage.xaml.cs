9151a29e0a6ec93014c8da15f3aaed54729f3c4e
Alex Chuang <1392alex1392@gmail.com>
2019-04-04 04:59:44 +0100

    complete chatlistpage

---
+++ CycChat/Views/ChatListPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CycChat
+{
+  /// <summary>
+  /// ChatListPage.xaml 的互動邏輯
+  /// </summary>
+  public partial class ChatListPage : Page
+  {
+    public ChatListPage()
+    {
+      InitializeComponent();
+      DataContext = DI.mainViewModel;
+    }
+
+    private void NewChatButton_Click(object sender, RoutedEventArgs e)
+    {
+      var dialog = new NewChatDialog();
+      if ((bool)dialog.ShowDialog())
+      {
+        // open chat window with dialog.FriendName
+      }
+    }
+  }
+}

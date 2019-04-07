91b2b228894bc3283dd079f38931adb1d9545d5d
Alex Chuang <1392alex1392@gmail.com>
2019-04-07 04:26:19 +0100

    add viewmodels

---
+++ CycChat.Core/ViewModels/ChatDialogViewModel.cs
@@ -0,0 +1,49 @@
+﻿using CycWpfLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+
+namespace CycChat.Core
+{
+  public class ChatDialogViewModel : ViewModelBase
+  {
+    public readonly object newChatToken = new object();
+
+    public List<string> FriendNames => UserDataModel.FriendNames;
+
+    public ChatDialogViewModel()
+    {
+      NewChatCommand = new RelayCommand(NewChatAsync, CanNewChat);
+    }
+
+    private bool CanNewChat() => IsViewValid;
+    private async void NewChatAsync()
+    {
+      if (!UserDataModel.ChatRooms.Any(r => r.FriendName == FriendName))
+      {
+        // Add new chat room
+        var userName = UserDataModel.Name;
+        var lastChat = UserDataModel.Chats.LastOrDefault(c => c.Sender == userName || c.Receiver == userName);
+        var chatRoom = new ChatRoom
+        {
+          FriendName = FriendName,
+          Owner = userName,
+          LastChat = lastChat.Content,
+          Time = lastChat.Time,
+        };
+        if (await UserDataModel.AddAsync<ChatRoom, AppDbContext>(chatRoom))
+        {
+          Messenger.Default.Send(new EventMessage<string>(FriendName), newChatToken);
+        }
+      }
+      else
+      {
+        Messenger.Default.Send(new EventMessage<string>(FriendName), newChatToken);
+      }
+    }
+
+    public string FriendName { get; set; }
+    public ICommand NewChatCommand { get; private set; }
+  }
+}
---
+++ CycChat.Core/ViewModels/ChatViewModel.cs
@@ -0,0 +1,29 @@
+﻿using CycWpfLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CycChat.Core
+{
+  public class ChatViewModel : ViewModelBase
+  {
+    public RelayCommand SendCommand { get; private set; }
+    public string Chat { get; set; }
+    public List<Chat> Chats { get; set; }
+
+    public ChatViewModel()
+    {
+      SendCommand = new RelayCommand(Send, CanSend);
+    }
+
+    private void Send()
+    {
+      // add new chat to userdata.allchats
+
+    }
+
+    private bool CanSend() => !string.IsNullOrEmpty(Chat);
+  }
+}

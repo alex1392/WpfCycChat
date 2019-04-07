e1ee33e416be449d6b1fc5edb98376eac7ebf020
Alex Chuang <1392alex1392@gmail.com>
2019-04-07 04:25:43 +0100

    add datamodels

---
+++ CycChat.Core/Model/DataModel.cs
@@ -0,0 +1,35 @@
+﻿using CycWpfLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CycChat.Core
+{
+  public class DataModel : DataModelBase<DataModel>
+  {
+    static DataModel()
+    {
+      InitializeAsync();
+    }
+
+    public static async void InitializeAsync()
+    {
+      Users = await DbService<AppDbContext>.GetAsync<User>();
+    }
+
+    private static List<User> users;
+    public static List<User> Users
+    {
+      get => users;
+      set
+      {
+        users = value;
+        OnStaticPropertyChanged(nameof(DataModel), nameof(Users));
+      }
+    }
+
+  }
+}
---
+++ CycChat.Core/Model/UserDataModel.cs
@@ -0,0 +1,44 @@
+﻿using CycWpfLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CycChat.Core
+{
+  /// <summary>
+  /// Provide user data from database and the services of munipulating data
+  /// </summary>
+  public class UserDataModel : DataModelBase<UserDataModel>
+  {
+    private static string username;
+    public static string Name
+    {
+      get => username;
+      set
+      {
+        username = value;
+        InitializeAsync();
+      }
+    }
+
+    private static async void InitializeAsync()
+    {
+      var friends = await DbService<AppDbContext>.GetAsync<Friend>();
+      var chats = await DbService<AppDbContext>.GetAsync<Chat>();
+      var chatRooms = await DbService<AppDbContext>.GetAsync<ChatRoom>();
+
+      FriendNames = friends.Where(f => f.A == username).Select(f => f.B)
+        .Concat(friends.Where(f => f.B == username).Select(f => f.A))
+        .ToList();
+      Chats = chats.Where(c => c.Sender == username || c.Receiver == username).ToList();
+      ChatRooms = chatRooms.Where(r => r.Owner == username).ToList();
+    }
+
+    public static List<string> FriendNames { get; private set; }
+
+    public static List<Chat> Chats { get; private set; }
+
+    public static List<ChatRoom> ChatRooms { get; private set; }
+  }
+}

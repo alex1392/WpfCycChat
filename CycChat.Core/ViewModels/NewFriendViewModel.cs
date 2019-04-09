9044e4619941fe4c351134b5a7c754cf110ecd92
Alex Chuang <1392alex1392@gmail.com>
2019-04-09 04:57:26 +0100

    add new friend dialog

---
+++ CycChat.Core/ViewModels/NewFriendViewModel.cs
@@ -0,0 +1,74 @@
+﻿using CycWpfLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CycChat.Core
+{
+  public class NewFriendViewModel : ViewModelBase
+  {
+    public NewFriendViewModel()
+    {
+      AddFriendCommand = new RelayCommand(AddFriendAsync, CanAddFriend);
+    }
+
+    private List<string> UserNames => DataModel.UserNames;
+    private List<Friend> AllFriends => DataModel.Friends;
+    private string UserName => UserDataModel.Name;
+    public RelayCommand AddFriendCommand { get; private set; }
+    public string FriendName { get; set; }
+    public bool IsFound { get; set; }
+
+    private string _searchName;
+    public readonly object messageToken = new object();
+    public readonly object closeToken = new object();
+
+    public string SearchName
+    {
+      get => _searchName;
+      set
+      {
+        _searchName = value;
+        Search();
+      }
+    }
+
+    private bool CanAddFriend()
+    {
+      return IsFound;
+    }
+    private async void AddFriendAsync()
+    {
+      // if they are already friends
+      if (AllFriends.Any(f => f.A == UserName && f.B == FriendName
+      || f.A == FriendName && f.B == UserName))
+      {
+        Messenger.Default.Send(new Message<string>("You are already friends."), messageToken);
+        return;
+      }
+
+      // TODO: send a friend request
+      // add friend immediately
+      if (await DataModel.AddAsync<Friend, AppDbContext>(new Friend(UserName, FriendName)))
+      {
+        Messenger.Default.Send(EventMessage.Empty, closeToken);
+      }
+    }
+
+    private void Search()
+    {
+      if (UserNames.Contains(_searchName))
+      {
+        IsFound = true;
+        FriendName = _searchName;
+      }
+      else
+      {
+        IsFound = false;
+        FriendName = "User not found.";
+      }
+    }
+  }
+}
---
+++ CycChat/Views/NewFriendDialog.xaml.cs
@@ -0,0 +1,18 @@
+﻿using CycWpfLibrary;
+using System.Windows;
+
+namespace CycChat
+{
+  /// <summary>
+  /// NewFriendDialog.xaml 的互動邏輯
+  /// </summary>
+  public partial class NewFriendDialog : Window
+  {
+    public NewFriendDialog()
+    {
+      InitializeComponent();
+      DataContext = DI.NewFriendVM;
+      Messenger.Default.Register<Message<string>>(this, DI.NewFriendVM.messageToken, msg => MessageBox.Show(msg.Content));
+    }
+  }
+}

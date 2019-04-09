6a4439859bd9dbb03c64ddc874131bfc39ff9d29
Alex Chuang <1392alex1392@gmail.com>
2019-04-09 05:00:35 +0100

    auto update datamodel, propagate propertychanged events

--- CycChat.Core/Model/DataModel.cs
+++ CycChat.Core/Model/DataModel.cs
@@ -2,34 +2,97 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace CycChat.Core
 {
+  /// <summary>
+  /// Provide data directly extracted from database.
+  /// </summary>
   public class DataModel : DataModelBase<DataModel>
   {
     static DataModel()
     {
-      InitializeAsync();
+      var timer = new DispatcherTimer(
+        TimeSpan.FromSeconds(1),
+        DispatcherPriority.Background,
+        async (sender, e) => await InitializeAsync(),
+        Application.Current.Dispatcher);
+      timer.Start();
     }
 
-    public static async void InitializeAsync()
+    public static async Task InitializeAsync()
     {
       Users = await DbService<AppDbContext>.GetAsync<User>();
+      Friends = await DbService<AppDbContext>.GetAsync<Friend>();
+      Chats = await DbService<AppDbContext>.GetAsync<Chat>();
+      ChatRooms = await DbService<AppDbContext>.GetAsync<ChatRoom>();
     }
 
-    private static List<User> users;
+    private static List<User> users = new List<User>();
+    private static List<Friend> friends = new List<Friend>();
+    private static List<ChatRoom> chatRooms = new List<ChatRoom>();
+    private static List<Chat> chats = new List<Chat>();
+
+
     public static List<User> Users
     {
       get => users;
       set
       {
-        users = value;
-        OnStaticPropertyChanged(nameof(DataModel), nameof(Users));
+        if (!value.DataEquals(users))
+        {
+          users = value;
+          OnStaticPropertyChanged(nameof(DataModel), nameof(Users));
+        }
       }
     }
 
+    public static List<Friend> Friends
+    {
+      get => friends;
+      set
+      {
+        if (!value.DataEquals(friends))
+        {
+          friends = value;
+          OnStaticPropertyChanged(nameof(DataModel), nameof(Friends));
+        }
+      }
+    }
+
+    public static List<Chat> Chats
+    {
+      get => chats;
+      private set
+      {
+        if (!value.DataEquals(chats))
+        {
+          chats = value;
+          OnStaticPropertyChanged(nameof(DataModel), nameof(Chats));
+        }
+      }
+    }
+
+    public static List<ChatRoom> ChatRooms
+    {
+      get => chatRooms;
+      private set
+      {
+        if (!value.DataEquals(chatRooms))
+        {
+          chatRooms = value;
+          OnStaticPropertyChanged(nameof(DataModel), nameof(ChatRooms));
+        }
+      }
+    }
+
+    public static List<string> UserNames => users.Select(u => u.UserName).ToList();
   }
 }
--- CycChat.Core/Model/UserDataModel.cs
+++ CycChat.Core/Model/UserDataModel.cs
@@ -7,38 +7,118 @@ using System.Threading.Tasks;
 namespace CycChat.Core
 {
   /// <summary>
-  /// Provide user data from database and the services of munipulating data
+  /// Provide user related data.
   /// </summary>
-  public class UserDataModel : DataModelBase<UserDataModel>
+  public class UserDataModel : ObservableObject
   {
     private static string username;
+    private static List<string> friendNames = new List<string>();
+    private static List<ChatRoom> chatRooms = new List<ChatRoom>();
+    private static List<Chat> chats = new List<Chat>();
+
+    static UserDataModel()
+    {
+      StaticPropertyChanged += UserDataModel_StaticPropertyChanged;
+    }
+
+    private static void UserDataModel_StaticPropertyChanged(object sender, StaticPropertyChangedEventArgs e)
+    {
+      if (e.ClassName == nameof(DataModel))
+      {
+        switch (e.PropertyName)
+        {
+          case nameof(DataModel.Friends):
+            Initialize(nameof(FriendNames));
+            break;
+          case nameof(DataModel.ChatRooms):
+            Initialize(nameof(ChatRooms));
+            break;
+          case nameof(DataModel.Chats):
+            Initialize(nameof(Chats));
+            break;
+          default:
+            break;
+        }
+      }
+    }
+
+    private static void Initialize(string propertyName)
+    {
+      switch (propertyName)
+      {
+        case nameof(FriendNames):
+          FriendNames = DataModel.Friends
+            .Where(f => f.A == username)
+            .Select(f => f.B)
+            .Concat(DataModel.Friends
+                .Where(f => f.B == username)
+                .Select(f => f.A))
+            .ToList();
+          break;
+        case nameof(Chats):
+          Chats = DataModel.Chats.Where(
+            c => c.Sender == username || c.Receiver == username).ToList();
+          break;
+        case nameof(ChatRooms):
+          ChatRooms = DataModel.ChatRooms.Where(
+            r => r.Owner == username).ToList();
+          break;
+        default:
+          break;
+      }
+
+    }
+
     public static string Name
     {
       get => username;
       set
       {
         username = value;
-        InitializeAsync();
+        Initialize(nameof(FriendNames));
+        Initialize(nameof(Chats));
+        Initialize(nameof(ChatRooms));
       }
     }
 
-    private static async void InitializeAsync()
+    public static List<Chat> Chats
     {
-      var friends = await DbService<AppDbContext>.GetAsync<Friend>();
-      var chats = await DbService<AppDbContext>.GetAsync<Chat>();
-      var chatRooms = await DbService<AppDbContext>.GetAsync<ChatRoom>();
-
-      FriendNames = friends.Where(f => f.A == username).Select(f => f.B)
-        .Concat(friends.Where(f => f.B == username).Select(f => f.A))
-        .ToList();
-      Chats = chats.Where(c => c.Sender == username || c.Receiver == username).ToList();
-      ChatRooms = chatRooms.Where(r => r.Owner == username).ToList();
+      get => chats;
+      private set
+      {
+        if (!value.DataEquals(chats))
+        {
+          chats = value;
+          OnStaticPropertyChanged(nameof(UserDataModel), nameof(Chats));
+        }
+      }
     }
 
-    public static List<string> FriendNames { get; private set; }
+    public static List<ChatRoom> ChatRooms
+    {
+      get => chatRooms;
+      private set
+      {
+        if (!value.DataEquals(chatRooms))
+        {
+          chatRooms = value;
+          OnStaticPropertyChanged(nameof(UserDataModel), nameof(ChatRooms));
+        }
+      }
+    }
 
-    public static List<Chat> Chats { get; private set; }
+    public static List<string> FriendNames
+    {
+      get => friendNames;
+      private set
+      {
+        if (!value.SequenceEqual(friendNames))
+        {
+          friendNames = value;
+          OnStaticPropertyChanged(nameof(UserDataModel), nameof(FriendNames));
+        }
+      }
+    }
 
-    public static List<ChatRoom> ChatRooms { get; private set; }
   }
 }
